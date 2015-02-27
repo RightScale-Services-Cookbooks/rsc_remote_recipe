e716f2e14d20f3f350c4d1e7bc09c060470e0766
Curt Wilhelm <curt@rightscale.com>
2015-02-26 17:49:18 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "rsc_remote_recipe"
+maintainer       "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+license          "Apache 2.0"
+description      "Runs rightscale remote recipes" 
+version          "0.1.0"
+
+recipe "rsc_remote_recipe", "runs  remote recipts" 
---
+++ resources/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: rsc_remote_recipe
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+
+
+# remote_recipe resources
+attribute :recipe, :kind_of => [String], :default => ""
+attribute :recipient_tags, :kind_of => [String,Array], :default => ""
+attribute :attributes, :kind_of => [Hash,NilClass], :default => nil
+
+actions :run
