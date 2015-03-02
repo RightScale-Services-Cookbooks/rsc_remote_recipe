0d3dcfc073a1f699f788620dbe960d3ad138c95e
Curt Wilhelm <curt@rightscale.com>
2015-03-02 15:50:34 -0700

    change recipe value

--- providers/default.rb
+++ providers/default.rb
@@ -1,9 +1,7 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+
 
 action :run do
-  remote_recipe =  new_resource.recipe || new_resource.name
+  remote_recipe =  new_resource.recipe 
   tags = new_resource.recipient_tags
   attributues = new_resource.attributes || {}
   remote_request_json ="/tmp/rsc_remote_recipe-#{Time.now.to_i}.json"
