c2333f14505d3317de328775e98f550e054e3ea7
Curt Wilhelm <curt@rightscale.com>
2017-03-02 17:48:53 -0700

    updating version and changelog

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'RightScale, Inc.'
 maintainer_email 'support@rightscale.com'
 license          'Apache 2.0'
 description      'Runs rightscale remote recipes'
-version          '10.1.0'
+version          '10.1.1'
 issues_url 'https://github.com/RightScale-Services-Cookbooks/rsc_remote_recipe/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/RightScale-Services-Cookbooks/rsc_remote_recipe' if respond_to?(:source_url)
 chef_version '>= 12.0' if respond_to?(:chef_version)
